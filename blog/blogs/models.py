50225e7d2b6ec0e1ea853084008b04943c9ae8a5
Sadanun <Sadanunborpit@gmail.com>
2026-04-22 17:19:05 +0700

    New Hire #5 Fix : Remove DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

--- blog/blogs/models.py
+++ blog/blogs/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
 from django.db import models
-from django.utils import timezone
 
 
 # Create your models here.
--- blog/users/apps.py
+++ blog/users/apps.py
@@ -3,7 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "blog.users"
     verbose_name = _("Users")
 
