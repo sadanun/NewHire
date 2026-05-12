5f4194937f2500ada72f56e91e97cf1e0123cc66
Sadanun <Sadanunborpit@gmail.com>
2026-05-12 15:48:57 +0700

    New Hire #16 Use  ModelViewSet

--- blog/blog_api/views.py
+++ blog/blog_api/views.py
@@ -1,53 +1,40 @@
-from rest_framework import mixins, viewsets
+from rest_framework import viewsets
 
 from blog.blogs.models import Category, Comment, Post
 
 from .serializers import CategorySerializer, CommentSerializer, PostSerializer
 
 
-class PostListViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet,
-):
+class PostListViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    http_method_names = ["get", "post", "put"]
 
 
-class CategoryListViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet,
-):
+class CategoryListViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    http_method_names = ["get", "post", "put"]
 
 
-class CommentListViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet,
-):
+class CommentListViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
+    http_method_names = ["get", "post", "put"]
     serializer_class = CommentSerializer
 
 
-class CommentListInPostViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class CommentListInPostViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    http_method_names = ["get"]
 
     def get_queryset(self):
         post_id = self.kwargs["post_id"]
         return Comment.objects.filter(post_id=post_id)
 
 
-class AllBlogBelongsToCategoryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class AllBlogBelongsToCategoryViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
+    http_method_names = ["get"]
 
     def get_queryset(self):
         category_id = self.kwargs["category_id"]
